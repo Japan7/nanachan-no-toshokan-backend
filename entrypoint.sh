99c1ba08267ddcbdc676ac0aaca60678809fb150
mistiru <dev@mistiru.me>
2022-07-23 12:33:56 +0200

    Use 2 spaces in shell scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ set -e
 ./manage.py collectstatic -c --no-input
 
 if [ $# -ge 1 ] && [ "$1" = "test" ]; then
-    ./manage.py makemigrations --check --dry-run
-    ./manage.py check
-    exec python -Wa manage.py test
+  ./manage.py makemigrations --check --dry-run
+  ./manage.py check
+  exec python -Wa manage.py test
 fi
 
 ./manage.py migrate
 ./manage.py remove_stale_contenttypes --include-stale-apps --no-input
 
 if [ $# -ge 1 ] && [ "$1" = "debug" ]; then
-    exec python -Wa manage.py runserver 0.0.0.0:8000
+  exec python -Wa manage.py runserver 0.0.0.0:8000
 else
-    ./manage.py check --deploy --fail-level=WARNING
-    exec gunicorn
+  ./manage.py check --deploy --fail-level=WARNING
+  exec gunicorn
 fi
