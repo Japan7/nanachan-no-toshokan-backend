c7063d75628fd16c1d7e3185c9e2cbe364dbbee5
mistiru <dev@mistiru.me>
2022-07-23 12:24:53 +0200

    Add production setting in local_settings
    Also remove useless comments.

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,10 +1,9 @@
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+USE_X_FORWARDED_PORT = False
 
 # Databases
 
